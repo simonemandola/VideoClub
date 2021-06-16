c9f33705d0afae7147ed659391036c5cab81bc12
simonemandola <s.mandola.design@hotmail.com>
2021-06-16 09:42:04 +0200

    menu mobile y menu desk
    He arreglado el menu el la versión mobile y desktop añadiendo el buscador

--- assets/js/code.js
+++ assets/js/code.js
@@ -4,6 +4,9 @@ const MENU_COLLAPSE = document.querySelector('.menu-collapse');
 const BTN_SEARCH = document.querySelector('#btnSearch');
 const SEARCH_COLLAPSE = document.querySelector('.search-collapse');
 
+// Variables
+let viewportWidth = window.matchMedia("(min-width: 768px)");
+
 // Añade el evento Listener al botón del menú
 BTN_MENU.addEventListener("click", openMenu);
 
@@ -16,24 +19,41 @@ function openMenu() {
         MENU_COLLAPSE.classList.replace('h-0', 'h-screen');
         MENU_COLLAPSE.classList.add('z-50');
         SEARCH_COLLAPSE.classList.replace('h-screen', 'h-0');
-        MENU_COLLAPSE.classList.remove('opacity-0');
+        MENU_COLLAPSE.classList.remove('sm:opacity-0');
     } else {
         MENU_COLLAPSE.classList.replace('h-screen', 'h-0');
         MENU_COLLAPSE.classList.remove('z-50');
-        MENU_COLLAPSE.classList.add('opacity-0');
+        MENU_COLLAPSE.classList.add('sm:opacity-0');
     }
 }
 
 // Función que abre o cierra el menú de navegación el la versión mobile
 function openSearch() {
+
+    if(viewportWidth.matches) {
+        openSearchDesktop();
+    } else {
+        openSearchMobile();
+    }
+}
+
+function openSearchMobile() {
     if(SEARCH_COLLAPSE.classList.contains("h-0")) {
         SEARCH_COLLAPSE.classList.replace('h-0', 'h-screen');
         SEARCH_COLLAPSE.classList.add('z-50');
-        SEARCH_COLLAPSE.classList.remove('opacity-0');
         MENU_COLLAPSE.classList.replace('h-screen', 'h-0');
     } else {
         SEARCH_COLLAPSE.classList.replace('h-screen', 'h-0');
         SEARCH_COLLAPSE.classList.remove('z-50');
-        SEARCH_COLLAPSE.classList.add('opacity-0');
+    }
+}
+
+function openSearchDesktop() {
+    if(SEARCH_COLLAPSE.classList.contains("md:w-0")) {
+        SEARCH_COLLAPSE.classList.replace('md:w-0', 'md:w-96');
+        SEARCH_COLLAPSE.classList.replace('md:pl-0', 'md:pl-4');
+    } else {
+        SEARCH_COLLAPSE.classList.replace('md:w-96', 'md:w-0');
+        SEARCH_COLLAPSE.classList.replace('md:pl-4', 'md:pl-0');
     }
 }
